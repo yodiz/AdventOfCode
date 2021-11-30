9574048e2389fd5ffcd5e4a4cdcba2a142489c18
Micke <mikael.kjellqvist@gmail.com>
2021-11-30 17:27:16 +0100

    Day1 preparations

---
+++ 2021/Day1/Main.fsx
@@ -0,0 +1,22 @@
+﻿#if INTERACTIVE
+#load "../Common.fsx"
+#endif
+
+open System
+open AoC
+
+let folder = __SOURCE_DIRECTORY__ + "\\"
+
+let input  = load folder "input.txt"
+
+let part1 = 
+    0
+
+let part2 = 
+    0
+
+[<EntryPoint>]
+let main argv =
+    printfn "Part1: %i" part1
+    printfn "Part2: %i" part2
+    0
---
+++ 2021/_template/Main.fsx
@@ -0,0 +1,20 @@
+﻿#if INTERACTIVE
+#load "../Common.fsx"
+#endif
+
+open System
+open AoC
+
+let input  = load "input.txt"
+
+let part1 = 
+    0
+
+let part2 = 
+    0
+
+[<EntryPoint>]
+let main argv =
+    printfn "Part1: %i" part1
+    printfn "Part2: %i" part2
+    0
