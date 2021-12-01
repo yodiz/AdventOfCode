053b1d77dcd8f564c77f51d61f8e8cbab383ee23
Micke <mikael.kjellqvist@gmail.com>
2021-12-01 08:29:54 +0100

    Day1

--- 2021/Day1/Main.fsx
+++ 2021/Day1/Main.fsx
@@ -2,18 +2,27 @@
 #load "../Common.fsx"
 #endif
 
+//START: 08:11 - 08:16 - 8:23 - inklusive påklädning av barn
+
 open System
 open AoC
 
 let folder = __SOURCE_DIRECTORY__ + "\\"
+let input  = load folder "input.txt" |> Array.map (Parse.int32)
 
-let input  = load folder "input.txt"
+let countDec arr = 
+    arr
+    |> Array.fold 
+        (fun (p,s) x -> (Some x, match p with |Some p when x > p -> s+1 |_ -> s )) 
+        (None, 0)
+    |> snd
 
-let part1 = 
-    0
-
+let part1 = countDec input
 let part2 = 
-    0
+    input
+    |> Array.windowed 3
+    |> Array.map Array.sum
+    |> countDec
 
 [<EntryPoint>]
 let main argv =
