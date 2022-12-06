dfeb085d2389551b0747d78a1ea97a3c880fb8b7
Mikael Kjellqvist <mikael.kjellqvist@gmail.com>
2022-12-06 21:47:49 +0100

    Quick & dirty 2 & 3

--- 2022/Day2/Main.fsx
+++ 2022/Day2/Main.fsx
@@ -7,16 +7,65 @@ module Day2
 open System
 open AoC
 
+type Sign = |Rock|Paper|Scissor
+type Out = |Loss|Win|Draw
+
+let ofLetter = function |"A"|"X" -> Rock |"B"|"Y" -> Paper |"C"|"Z" -> Scissor |a -> failwithf "Unsupported %s" a
+let score = function |Rock -> 1 |Paper -> 2 |Scissor -> 3
 let folder = __SOURCE_DIRECTORY__ + "\\"
 
+let ofOut = function |"X" -> Loss |"Y" -> Draw |"Z" -> Win |a -> failwithf "Unsupported %s" a
+
+
+let input  = load folder "input.txt" 
+
 let parseLine (str:string) = 
-    ()
+    let a,b = AoC.Text.split2 " " str
+    ofLetter a, ofLetter b
 
-let input  = load folder "input.txt" |> Array.map parseLine
+let outScore = function |Win -> 6 |Loss -> 0 |Draw -> 3
 
-let part1 = 0
+let part1 =     
+    input 
+    |> Array.map parseLine
+    |> Array.map 
+        (fun (o,m) ->  
+            let w = 
+                match o,m with 
+                |Rock, Scissor -> Loss
+                |Paper, Rock -> Loss
+                |Scissor, Paper -> Loss 
+                |Scissor, Rock -> Win
+                |Rock, Paper -> Win
+                |Paper, Scissor -> Win
+                |_ -> Draw
+            outScore w + score m
+        )
+    |> Array.sum
 
-let part2 = 0
+let parseLine2 (str:string) = 
+    let a,b = AoC.Text.split2 " " str
+    ofLetter a, ofOut b
+
+let part2 = 
+    input 
+    |> Array.map parseLine2
+    |> Array.map 
+        (fun (o,w) ->  
+            let m = 
+                match o,w with 
+                |Rock, Win -> Paper
+                |Rock, Loss -> Scissor
+                |Rock, Draw -> Rock
+                |Paper, Win -> Scissor
+                |Paper, Loss -> Rock
+                |Paper, Draw -> Paper
+                |Scissor, Win -> Rock
+                |Scissor, Loss -> Paper
+                |Scissor, Draw -> Scissor
+            outScore w + score m
+        )
+    |> Array.sum
 
 
 //
---
+++ 2022/Day3/Main.fsx
@@ -0,0 +1,49 @@
+﻿#if INTERACTIVE
+#load "../Common.fsx"
+#else
+module Day3
+#endif
+
+open System
+open AoC
+
+let folder = __SOURCE_DIRECTORY__ + "\\"
+
+let input  = load folder "input.txt" |> Array.map id
+
+let score (x:char) = if System.Char.IsLower x then int x - 96 else int x - 38
+
+let part1 = 
+    input
+    |> Array.map
+        (fun (str) -> 
+            let l = str.Length / 2
+            let a = str.Substring(0, l)
+            let b = str.Substring(l)
+            let sa = a.ToCharArray() |> Set.ofArray
+            let sb = b.ToCharArray() |> Set.ofArray
+            let s = Set.intersect sa sb
+            Set.fold (fun _ x -> score x) 0 s
+        )
+    |> Array.sum
+
+[|0..10|] |> Array.chunkBySize 3
+
+let part2 = 
+    input
+    |> Array.chunkBySize 3
+    |> Array.map
+        (fun (group) -> 
+            let a = group.[0].ToCharArray() |> Set.ofArray
+            let b = group.[1].ToCharArray() |> Set.ofArray
+            let c = group.[2].ToCharArray() |> Set.ofArray
+            let r = Set.intersectMany [a;b;c]
+            Set.fold (fun _ x -> score x) 0 r
+        )
+    |> Array.sum
+
+
+//
+printfn "%i" part1
+printfn "%i" part2
+
