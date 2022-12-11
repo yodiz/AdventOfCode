80008726c69da9bf239cadcfa3b55bcbd7e7d58b
Mikael Kjellqvist <mikael.kjellqvist@gmail.com>
2022-12-11 21:09:19 +0100

    Day5 hack

--- 2022/Day4/Main.fsx
+++ 2022/Day4/Main.fsx
@@ -9,14 +9,27 @@ open AoC
 
 let folder = __SOURCE_DIRECTORY__ + "\\"
 
+let isRange ((x1,x2),(y1,y2)) = 
+    (x1 >= y1 && x2 <= y2)
+    || (y1 >= x1 && y2 <= x2)
+
+
+let isOverlap ((x1,x2),(y1,y2)) = 
+    x1 <= y2 && x2 >= y1
+
+
+//14-83,5-14
 let parseLine (str:string) = 
-    ()
+    let a,b = AoC.Text.split2 "," str
+    let p f = let a,b = AoC.Text.split2 "-" f in a |> Parse.int32, b |> Parse.int32
+    let a, b = p a , p b
+    a,b
 
 let input  = load folder "input.txt" |> Array.map parseLine
 
-let part1 = 0
+let part1 = input |> Array.filter (isRange) |> Array.length
 
-let part2 = 0
+let part2 = input |> Array.filter (isOverlap) |> Array.length
 
 
 //
