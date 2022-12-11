136ac25833b753bbd31274aef321b735b602dd98
Mikael Kjellqvist <mikael.kjellqvist@gmail.com>
2022-12-11 21:29:20 +0100

    Day7 hack

--- 2022/Day5/Main.fsx
+++ 2022/Day5/Main.fsx
@@ -12,14 +12,114 @@ let folder = __SOURCE_DIRECTORY__ + "\\"
 let parseLine (str:string) = 
     ()
 
-let input  = load folder "input.txt" |> Array.map parseLine
+let input  = 
+    load folder "input.txt" 
 
-let part1 = 0
+let i = input |> Array.findIndex ((=)"")
 
-let part2 = 0
+let board = 
+    let mutable d = Map.empty
+    input[0..i-2]
+    |> Array.iter 
+        (fun x -> 
+            x.ToCharArray()
+            |> Array.mapi (fun i x -> if (i - 1) % 4 = 0 then Some x else None)
+            |> Array.choose id
+            |> Array.iteri (fun i x -> if x <> ' ' then match d |> Map.tryFind (i+1) with |None -> d <- d |> Map.add (i+1) [x] |Some l -> d <- d |> Map.add (i+1) (x::l))
+        )
+    d |> Map.map (fun k v -> v |> List.rev)
+
+
+let printBoard() = 
+    board 
+    |> Map.toArray 
+    |> Array.map snd 
+    |> Array.map (fun x -> x |> List.map (string) |> String.concat "") 
+    |> Array.map string 
+    |> String.concat " "
+
+printfn "%s %A" (printBoard ()) board
+
+//move 2 from 2 to 8
+let moves = 
+    input[i+1..]
+    |> Array.map (fun x -> Regexp.matchOrFail "move (\\d+) from (\\d+) to (\\d)" x)
+    |> Array.fold
+        (fun (board:Map<_,_>) x -> 
+            match x with 
+            |[Int32 n;Int32 f;Int32 t] -> 
+                printfn "%i from %i to %i" n f t 
+
+
+                let things, a = 
+                    [0..n-1] 
+                    |> List.fold
+                        (fun (things, s) _ -> 
+                            let x::t = s |> Map.find f 
+                            x::things, s |> Map.add f t
+                        ) 
+                        ([], board)
+                
+                let xx = 
+                    a |> Map.add t (List.append  (things) (a |> Map.find t))
+                
+                printfn "%s %A" (printBoard ()) xx
+                xx
+
+        )
+        board
+
+
+
+//move 2 from 2 to 8
+let moves2 = 
+    input[i+1..]
+    |> Array.map (fun x -> Regexp.matchOrFail "move (\\d+) from (\\d+) to (\\d)" x)
+    |> Array.fold
+        (fun (board:Map<_,_>) x -> 
+            match x with 
+            |[Int32 n;Int32 f;Int32 t] -> 
+                printfn "%i from %i to %i" n f t 
+
+
+                let things, a = 
+                    [0..n-1] 
+                    |> List.fold
+                        (fun (things, s) _ -> 
+                            let x::t = s |> Map.find f 
+                            x::things, s |> Map.add f t
+                        ) 
+                        ([], board)
+                
+                let xx = 
+                    a |> Map.add t (List.append  (things |> List.rev) (a |> Map.find t))
+                
+                printfn "%s %A" (printBoard ()) xx
+                xx
+
+        )
+        board
+
+
+let part1 = 
+    moves
+    |> Map.toArray 
+    |> Array.map snd 
+    |> Array.map (fun x -> let a = x |> List.toArray in a.[0]) 
+    |> Array.map string 
+    |> String.concat ""
+   
+
+let part2 = 
+    moves2
+    |> Map.toArray 
+    |> Array.map snd 
+    |> Array.map (fun x -> let a = x |> List.toArray in a.[0]) 
+    |> Array.map string 
+    |> String.concat ""
 
 
 //
-printfn "%i" part1
-printfn "%i" part2
+printfn "%s" part1
+printfn "%s" part2
 
---
+++ 2022/Day6/Main.fsx
@@ -0,0 +1,41 @@
+﻿#if INTERACTIVE
+#load "../Common.fsx"
+#else
+module Day6
+#endif
+
+open System
+open AoC
+
+let folder = __SOURCE_DIRECTORY__ + "\\"
+
+let input  = loadAll folder "input.txt"
+
+
+let findMarker (arr:char array) (markLen:int) = 
+    arr
+    |> Array.mapi 
+        (fun i x -> 
+            if i >= markLen - 1 then
+                let l = 
+                    [0..markLen - 1]
+                    |> List.map (fun x -> arr.[i - x])
+                    |> Set.ofList
+                    |> Set.count
+                if l = markLen then 
+                    true
+                else false
+            else
+                false 
+        )
+    |> Array.findIndex (fun x -> x)
+    |> ((+)1)
+
+
+let part1 = findMarker (input.ToCharArray()) 4
+let part2 = findMarker (input.ToCharArray()) 14
+
+//
+printfn "%i" part1
+printfn "%i" part2
+
