faa8dba2e2b97bad5b4804f64976411939427c37
Mikael Kjellqvist <mikael.kjellqvist@gmail.com>
2023-01-09 20:13:36 +0100

    Day14 part 2

--- 2022/Day14/Main.fsx
+++ 2022/Day14/Main.fsx
@@ -34,39 +34,71 @@ let parseLine (str:string) =
                 else            [|for x = min x1 x2 to max x1 x2 do yield x,y1|]
             kuken
         )
-    |> Array.fold (fun s (x,y) -> s |> Map.add (x,y) (Some true)) Map.empty
+    |> Array.fold (fun s (x,y) -> s |> Map.add (x,y) (true)) Map.empty
     
 
-let rec tick (field:Map<int*int,bool option>) = 
+let rec tick isTwo maxY (field:Map<int*int,bool>) = 
     //Drop sand, add to field and repeat
     let rec dropSand (sx,sy) =
-        //Move down, downleft, downright 
-        //if cant, place sand and proceed
-        //if below lowest piece, stop all
-        let d = 
-            [sx,sy+1; sx-1,sy+1; sx+1,sy+1]
-            |> List.tryPick (fun (x,y) -> field |> Map.tryFind (x,y))
-            
-        match d with 
-        |None -> 
-
+        if isTwo = false && sy >= maxY then
+            None
+        else
+            //Move down, downleft, downright 
+            //if cant, place sand and proceed
+            //if below lowest piece, stop all
+            let d = 
+                [sx,sy+1; sx-1,sy+1; sx+1,sy+1]
+                |> List.tryPick 
+                    (fun (x,y) -> 
+                        if isTwo && y = maxY + 2 then 
+                            None
+                        elif field |> Map.tryFind (x,y) |> Option.isNone then 
+                            Some (x,y) 
+                        else None
+                    )
         
-    dropSand (500,0)
+            match d with 
+            |Some (x,y) -> 
+                dropSand (x,y)
+            |None -> 
+                Some (sx,sy)
+
+    match dropSand (500,0) with 
+    |Some (x,y) when (x,y) = (500,0) -> (field |> Map.add (x,y) (false))
+    |Some (x,y) -> 
+        tick isTwo maxY (field |> Map.add (x,y) (false))
+    |None -> 
+        field
 
 
 
-let run1 filename = 
-    load folder filename 
-    |> Array.map parseLine
+let run isTwo filename = 
+    let orgField =
+        load folder filename 
+        |> Array.map parseLine
+        |> Array.fold 
+            (fun s x -> 
+                let a = s |> Map.toArray 
+                let b = x |> Map.toArray
+                Array.append a b |> Map.ofArray
+            ) 
+            Map.empty
 
-let test1 = run1 "test1.txt"
+    let maxY = 
+        orgField |> Map.fold (fun s (x,y) value -> max y s) 0
 
-let part1 = 0
+    let field = tick isTwo maxY orgField
+    field |> Map.fold (fun i (x,y) v -> if v then i else i + 1) 0
 
-let part2 = 0
+
+let test1 = run false "test1.txt"
+let test2 = run true "test1.txt"
+
+let part1 = run false "input.txt"
+let part2 = run true "input.txt"
 
 
-//
+
 printfn "%i" part1
 printfn "%i" part2
 
