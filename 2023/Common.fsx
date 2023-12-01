c3e1fdc49e4c7af8622532f061fefa74d5f87739
Mikael Kjellqvist <mikael.kjellqvist@gmail.com>
2023-12-01 15:05:44 +0100

    Day1 2023

---
+++ 2023/Common.fsx
@@ -0,0 +1,78 @@
+﻿[<AutoOpen>]
+module AoC
+
+let load folder file = System.IO.File.ReadAllLines(System.IO.Path.Combine(folder,file))
+let loadAll folder file = System.IO.File.ReadAllText(System.IO.Path.Combine(folder,file))
+
+module Parse = 
+    let parse fn (input:string) = match fn input with |true,value -> Some value |_ ->  None
+    let parseOrFail what fn input = parse fn input |> function |Some s -> s |None -> failwithf "%s not a %s" input what
+    let tryInt32 = parse System.Int32.TryParse
+    let int32 = parseOrFail "int32" System.Int32.TryParse
+    let tryInt64 = parse System.Int64.TryParse
+    let int64 = parseOrFail "int64" System.Int64.TryParse
+
+let (|Int32|_|) = Parse.tryInt32
+let (|Int64|_|) = Parse.tryInt64
+   
+module Array = 
+    let foldi<'state,'a> folder (s:'state) (arr:'a array) =
+        arr|> Array.fold (fun (i,s) x -> (i+1),folder i s x) (0,s) |> snd
+module List = 
+    let foldi<'state,'a> folder (s:'state) (arr:'a list) =
+        arr|> List.fold (fun (i,s) x -> (i+1),folder i s x) (0,s) |> snd
+
+
+module Map = 
+    ///Change existing value or use default value 
+    let changeOrDefault key change def map = 
+        map |> Map.add key (map |> Map.tryFind key |> function |Some s -> change s |None -> def)
+
+module Test = 
+    let equal<'a when 'a : equality> msg (expected:'a) (actual:'a) =
+        if expected = actual then
+            printfn "Success - %s" msg
+        else failwithf "Fail - %s expected %A, got %A" msg expected actual
+        
+
+module Trace = 
+    let mutable private traceEnable = false
+    let trace_on() = traceEnable <- true  
+    let trace_off() = traceEnable <- false  
+    let output str = if traceEnable then printfn "%s" str else ()
+    let withTrace fn = 
+        trace_on() 
+        fn() 
+        trace_off()
+
+
+module Text = 
+    let splitm_noempty (sepBy:string array) (str:string)  =
+       str.Split(sepBy, System.StringSplitOptions.RemoveEmptyEntries)
+    let splitm (sepBy:string array) (str:string)  =
+       str.Split(sepBy, System.StringSplitOptions.None)
+
+    let split_noempty (sepBy:string) (str:string)  = splitm_noempty [|sepBy|] str 
+    let split (sepBy:string) (str:string)  = splitm [|sepBy|] str 
+
+    let split2 sepBy string = 
+        match split sepBy string with 
+        |[|a;b|] -> a,b
+        |a -> failwithf "Expected two elements in '%s' Sep By '%s', but got %A"  string sepBy a
+
+    let trim (s:string) = s.Trim()
+    let trimc (c:char array) (s:string) = s.Trim(c)
+
+module Regexp = 
+    open System.Text.RegularExpressions
+
+    ///Pattern - Groups
+    let (|Regex|_|) pattern input =
+        let m = Regex.Match(input, pattern)
+        if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+        else None
+
+    let matchOrFail pattern input = 
+        match input with
+        |Regex pattern groups -> groups
+        |_ -> failwithf "Match %s failed on %s " pattern input
---
+++ 2023/Day1/Main.fsx
@@ -0,0 +1,44 @@
+﻿#if INTERACTIVE
+#load "../Common.fsx"
+#else 
+module Day1
+#endif
+
+let folder = __SOURCE_DIRECTORY__ + "\\"
+let input  = load folder "input.txt"
+let test1  = load folder "test1.txt"
+let test2  = load folder "test2.txt"
+    
+let indexes str cmp =      
+    let rec next (str:string) (substr:string) (loc:int) (indexes:int list) = 
+        let nxt = str.IndexOf(substr, loc)
+        if nxt = -1 then indexes
+        else next str cmp (nxt+1) (nxt::indexes)
+    next str cmp 0 []
+
+///Finds first and last <value> of input.
+/// Values are defined with list nums. Which defines a string and its corresponding value - ex: "one",1
+let getNums (nums) (inp:string) = 
+    let n = 
+        nums
+        |> List.collect (fun (str,value) -> indexes inp str |> List.map (fun i -> i, value))
+        |> List.sortBy fst
+        |> List.map snd
+    Parse.int32 (sprintf "%i%i" (n |> List.head) (n |> List.last))
+
+let solve nums input = input |> Array.map (getNums nums) |> Array.sum
+
+let nums1 = [0..9] |> List.map (fun x -> sprintf "%i" x, x)
+let nums2 = ["one",1;"two",2;"three",3;"four",4;"five",5;"six",6;"seven",7;"eight",8;"nine",9] 
+
+let p1 = solve nums1 
+let p2 = solve (List.append nums1 nums2)
+
+Test.equal "Test1" 142 (p2 test1)
+
+let part1 = p1 input
+let part2 = p2 input
+
+//
+printfn "Part1: %i" part1
+printfn "Part2: %i" part2
